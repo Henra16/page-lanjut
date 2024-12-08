69648bd832fa3338fbd9fc11c151e04377137465
Henra16 <henraiqranul16@gmail.com>
2024-12-08 19:22:53 +0800

    membuat halaman index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>selamat datang diwebsite saya</title>
+</head>
+<body>
+    <div>
+        <h1>Hai' Perkenalkan Saya Henra</h1>
+        <p>Saya yang membuat website ini</p>
+    </div>
+    
+</body>
+</html>
